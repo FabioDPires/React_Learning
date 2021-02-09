f4ce1c1577d2a18a2dff0e63b35ba0178838e2ee
Fábio <fabiopires1011@gmail.com>
2021-02-09 22:10:32 +0000

    some css added

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  //CSS
+  styles = {
+    fontSize: "10px",
+    fontWeight: "bold",
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </div>
+      <React.Fragment>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
